7c30068ea3d293c11b8a66b141b270a35d9535a6
Angel Alicea <ala31996@gmail.com>
2020-01-23 12:31:02 -0500

    working through 3 drill

--- Array.js
+++ Array.js
@@ -1,12 +1,11 @@
 const Memory = require('./memory');
 
-
-class Array {
+const memory = new Memory();
+class Array { 
   constructor() {
     this.length = 0;
     this._capacity = 0;
-    this.memory = new Memory();
-    this.ptr = this.memory.allocate(this.length);
+    this.ptr = memory.allocate(this.length);
   }
 
   push(value) {
@@ -15,18 +14,18 @@ class Array {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    this.memory.set(this.ptr + this.length, value);
+    memory.set(this.ptr + this.length, value);
     this.length++;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
-    this.ptr = this.memory.allocate(size);
+    this.ptr = memory.allocate(size);
     if (this.ptr === null) {
       throw new Error('Out of memory');
     }
-    this.memory.copy(this.ptr, oldPtr, this.length);
-    this.memory.free(oldPtr);
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
     this._capacity = size;
   }
 
@@ -34,14 +33,14 @@ class Array {
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    return this.memory.get(this.ptr + index);
+    return memory.get(this.ptr + index);
   }
 
   pop() {
     if (this.length == 0) {
       throw new Error('Index error');
     }
-    const value = this.memory.get(this.ptr + this.length - 1);
+    const value = memory.get(this.ptr + this.length - 1);
     this.length--;
     return value;
   }
@@ -55,8 +54,8 @@ class Array {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    this.memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-    this.memory.set(this.ptr + index, value);
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
     this.length++;
   }
 
@@ -64,7 +63,7 @@ class Array {
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    this.memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
     this.length--;
   }
 
--- array-drills.js
+++ array-drills.js
@@ -8,11 +8,14 @@ function main(){
 
   arr.push(3);
 //   arr.push(5);
-//   arr.push(15);
-//   arr.push(19);
-//   arr.push(45);
-//   arr.push(10);
+  // arr.push(15);
+  // arr.push(19);
+  // arr.push(45);
+  // arr.push(10);
 //-----do drill 3 here-----
+  arr.pop();
+  arr.pop();
+  arr.pop();
 
   console.log(arr);
 }
