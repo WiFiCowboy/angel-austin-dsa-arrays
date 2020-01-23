07274ad50145f9469f13e493d41bf88786df1baa
AustinT33 <austintumlinson@gmail.com>
2020-01-23 11:35:31 -0600

    drill 3 done

--- array-drills.js
+++ array-drills.js
@@ -5,14 +5,14 @@ function main(){
   Array.SIZE_RATIO = 3;
 
   let arr = new Array();
-
+//----drill 2----
   arr.push(3);
-//   arr.push(5);
-  // arr.push(15);
-  // arr.push(19);
-  // arr.push(45);
-  // arr.push(10);
-//-----do drill 3 here-----
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+//----drill 3-----
   arr.pop();
   arr.pop();
   arr.pop();
