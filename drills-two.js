f1908df203c71d634bfae4fafd53f7d8b0672e88
Angel Alicea <ala31996@gmail.com>
2020-01-23 14:32:08 -0500

    working through 6  drill

---
+++ drills-two.js
@@ -0,0 +1,16 @@
+// function urlify(str) {
+//   return str.trim().replace(/\s/g, "%20");
+// }
+
+// console.log(urlify("https://www.hello world .com"))
+
+function filterArray (arr) {
+  let newArr = []
+  for(let i=0; i < arr.length; i++){
+    if(arr.includes(5))
+    arr.push(newArr)
+  }
+  return newArr
+}
+
+console.log(filterArray([10, 20, 5, 3, 15, 5]))
